2ee87c3ee06f5dcb197dcc34edd347226fc78d65
bharathdev7 <156436638+bharathdev7@users.noreply.github.com>
2025-03-11 09:38:34 +0530

    revamp explore section

--- components/dashboard/explore/Explore-section.tsx
+++ components/dashboard/explore/Explore-section.tsx
@@ -1,38 +1,67 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
 import React, { useState } from 'react';
-import Map, { Marker, ViewState } from 'react-map-gl/mapbox';
+import Map, { Marker, ViewState, Popup } from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const popularPlaces = [
-    { name: 'Rock Beach', area: 'White Town', time: '6am -10pm', latitude: 11.9334, longitude: 79.8143, image: '/images/auroville.jpg' },
-    { name: 'Sand Beach', area: 'Black Town', time: '6am -10pm', latitude: 11.1234, longitude: 79.1233, image: '/images/auroville.jpg' },
-    { name: 'RockBeach', area: 'White Town', time: '6am -10pm', latitude: 11.9334, longitude: 79.8143, image: '/images/auroville.jpg' },
-    { name: 'Sand each', area: 'Black Town', time: '6am -10pm', latitude: 11.1234, longitude: 79.1233, image: '/images/auroville.jpg' },
-    { name: 'Rock Bach', area: 'White Town', time: '6am -10pm', latitude: 11.9334, longitude: 79.8143, image: '/images/auroville.jpg' },
-    { name: 'Sand Bech', area: 'Black Town', time: '6am -10pm', latitude: 11.1234, longitude: 79.1233, image: '/images/auroville.jpg' },
-];
+const categories = ['Beaches', 'Temples', 'Activities', 'Hotels', 'Restaurants'] as const;
+type Category = typeof categories[number];
+
+const placesByCategory = {
+    Beaches: [
+        { name: 'Paradise Beach', area: 'Chunnambar', time: '9am - 5pm', latitude: 11.8882, longitude: 79.8063, image: '/images/paradise-beach.jpg' },
+        { name: 'Promenade Beach', area: 'White Town', time: '24/7', latitude: 11.9321, longitude: 79.8351, image: '/images/promenade-beach.jpg' },
+        { name: 'Serenity Beach', area: 'Kottakuppam', time: '24/7', latitude: 11.9775, longitude: 79.8530, image: '/images/serenity-beach.jpg' },
+        { name: 'Auro Beach', area: 'Auroville', time: '24/7', latitude: 11.9715, longitude: 79.8427, image: '/images/auro-beach.jpg' },
+    ],
+    Temples: [
+        { name: 'Manakula Vinayagar Temple', area: 'White Town', time: '5:30am - 12:30pm & 4pm - 9pm', latitude: 11.9339, longitude: 79.8345, image: '/images/manakula-temple.jpg' },
+        { name: 'Varadaraja Perumal Temple', area: 'Heritage Town', time: '6am - 12pm & 4pm - 8pm', latitude: 11.9367, longitude: 79.8308, image: '/images/varadaraja-temple.jpg' },
+        { name: 'Vedhapureeswarar Temple', area: 'Heritage Town', time: '6am - 12pm & 4:30pm - 8:30pm', latitude: 11.9330, longitude: 79.8328, image: '/images/vedhapureeswarar-temple.jpg' },
+    ],
+    Activities: [
+        { name: 'Scuba Diving', area: 'Serenity Beach', time: '8am - 4pm', latitude: 11.9775, longitude: 79.8530, image: '/images/scuba.jpg' },
+        { name: 'Auroville Meditation Center', area: 'Auroville', time: '9am - 5pm', latitude: 12.0069, longitude: 79.8106, image: '/images/auroville.jpg' },
+        { name: 'Boat House', area: 'Chunnambar', time: '9am - 5pm', latitude: 11.8882, longitude: 79.8063, image: '/images/boat-house.jpg' },
+    ],
+    Hotels: [
+        { name: 'La Villa', area: 'White Town', time: '24/7', latitude: 11.9338, longitude: 79.8357, image: '/images/la-villa.jpg' },
+        { name: 'Palais de Mahe', area: 'White Town', time: '24/7', latitude: 11.9334, longitude: 79.8361, image: '/images/palais-mahe.jpg' },
+        { name: 'The Promenade', area: 'White Town', time: '24/7', latitude: 11.9325, longitude: 79.8361, image: '/images/promenade-hotel.jpg' },
+    ],
+    Restaurants: [
+        { name: 'Carte Blanche', area: 'White Town', time: '12pm - 10pm', latitude: 11.9339, longitude: 79.8357, image: '/images/carte-blanche.jpg' },
+        { name: 'La Villa Restaurant', area: 'White Town', time: '12pm - 10pm', latitude: 11.9338, longitude: 79.8357, image: '/images/la-villa-restaurant.jpg' },
+        { name: 'Surguru Restaurant', area: 'Heritage Town', time: '7am - 10:30pm', latitude: 11.9367, longitude: 79.8308, image: '/images/surguru.jpg' },
+    ],
+};
+
+const categoryImages = {
+    Beaches: '/images/beach-category.jpg',
+    Temples: '/images/temple-category.jpg',
+    Activities: '/images/activities-category.jpg',
+    Hotels: '/images/hotels-category.jpg',
+    Restaurants: '/images/restaurants-category.jpg',
+};
 
 const ExplorePage = () => {
+    const [selectedCategory, setSelectedCategory] = useState<Category>('Beaches');
+    const [selectedPlace, setSelectedPlace] = useState<null | {
+        name: string;
+        area: string;
+        time: string;
+        latitude: number;
+        longitude: number;
+        image: string;
+    }>(null);
     const [viewState, setViewState] = useState<ViewState>({
-        latitude: popularPlaces[0].latitude,
-        longitude: popularPlaces[0].longitude,
-        zoom: 14,
+        latitude: 11.9416,
+        longitude: 79.8083,
+        zoom: 12,
         bearing: 0,
         pitch: 0,
         padding: { top: 0, bottom: 0, left: 0, right: 0 }
     });
-
-    const [selectedPlace, setSelectedPlace] = useState(popularPlaces[0]);
-
-    const handlePlaceClick = (place: typeof popularPlaces[0]) => {
-        setSelectedPlace(place);
-        setViewState({
-            ...viewState,
-            latitude: place.latitude,
-            longitude: place.longitude,
-        });
-    };
 
     return (
         <div className="flex flex-col h-screen w-screen">
@@ -43,31 +72,61 @@ const ExplorePage = () => {
                 style={{ width: '100%', height: '100%' }}
                 mapStyle="mapbox://styles/mapbox/streets-v9"
             >
-                {popularPlaces.map((place) => (
+                {placesByCategory[selectedCategory].map((place) => (
                     <Marker
                         key={place.name}
                         latitude={place.latitude}
                         longitude={place.longitude}
                         anchor="bottom"
-                        color={selectedPlace === place ? '#ff0000' : '#000000'}
-                    />
+                        onClick={e => {
+                            e.originalEvent.stopPropagation();
+                            setSelectedPlace(place);
+                        }}
+                    >
+                    </Marker>
                 ))}
+
+                {selectedPlace && (
+                    <Popup
+                        latitude={selectedPlace.latitude}
+                        longitude={selectedPlace.longitude}
+                        anchor="bottom"
+                        onClose={() => setSelectedPlace(null)}
+                        closeButton={true}
+                    >
+                        <div className="p-2 max-w-[200px]">
+                            <img src={selectedPlace.image} alt={selectedPlace.name} 
+                                className="w-full h-32 object-cover rounded-lg mb-2" />
+                            <h3 className="font-bold text-sm">{selectedPlace.name}</h3>
+                            <p className="text-sm text-gray-600">{selectedPlace.area}</p>
+                            <p className="text-sm text-gray-600">{selectedPlace.time}</p>
+                        </div>
+                    </Popup>
+                )}
             </Map>
 
             <div className="w-full overflow-x-auto pb-28 md:pb-8 pt-2 px-4 no-scrollbar">
                 <div className="flex space-x-4 min-w-min">
-                    {popularPlaces.map(place => (
+                    {categories.map(category => (
                         <div 
-                            key={place.name} 
-                            className={`flex-shrink-0 bg-white rounded-lg shadow-lg p-4 flex w-[300px] cursor-pointer
-                                ${selectedPlace === place ? 'ring-2 ring-blue-500' : ''}`}
-                            onClick={() => handlePlaceClick(place)}
+                            key={category} 
+                            className={`flex-shrink-0 rounded-lg shadow-lg cursor-pointer overflow-hidden
+                                relative w-[200px] h-[120px] ${
+                                selectedCategory === category ? 'ring-2 ring-blue-500' : ''
+                            }`}
+                            onClick={() => setSelectedCategory(category)}
                         >
-                            <img src={place.image} alt={place.name} className="w-1/3 h-auto rounded-l-lg object-cover" />
-                            <div className="flex-1 p-2">
-                                <h3 className="font-bold text-lg">{place.name}</h3>
-                                <p>{place.area}</p>
-                                <p>{place.time}</p>
+                            <img 
+                                src={categoryImages[category]} 
+                                alt={category}
+                                className="absolute inset-0 w-full h-full object-cover"
+                            />
+                            <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-black/30" />
+                            <div className="absolute inset-0 p-4 flex flex-col justify-end">
+                                <h3 className="font-bold text-lg text-white">{category}</h3>
+                                <p className="text-sm text-gray-200">
+                                    {placesByCategory[category].length} places
+                                </p>
                             </div>
                         </div>
                     ))}
